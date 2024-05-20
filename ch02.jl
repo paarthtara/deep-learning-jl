24c299f5c46a302ebe1101256c2cbd64ed755fe6
paarthtara <paarthtara03@gmail.com>
2024-05-19 21:29:41 -0600

    added dummy file

---
+++ ch02.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.42
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e065826c-1658-11ef-11bd-ed46069ff1c0
+md"## Chapter 2: Linear Algebra"
+
+# ╔═╡ 3a333ee7-b8f6-481e-bf12-41ffb2f379a7
+# do something
+
+# ╔═╡ Cell order:
+# ╟─e065826c-1658-11ef-11bd-ed46069ff1c0
+# ╠═3a333ee7-b8f6-481e-bf12-41ffb2f379a7
